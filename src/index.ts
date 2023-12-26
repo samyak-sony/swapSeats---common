202210e7e63fc64c668b2f88fb11498f3fd5a9be
samyak-sony <sony.samyak@gmail.com>
2023-12-26 11:11:03 +0530

    Updates

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface Color{
+    red: number,
+    blue: number,
+    green: number;
+}
+
+const color: Color = {
+    red: 10,
+    blue: 20,
+    green: 20
+};
+
+console.log(color);
+export default color;

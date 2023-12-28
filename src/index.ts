4908e2eb4acc70c8d5a91df61966e66b80c98d44
samyak-sony <sony.samyak@gmail.com>
2023-12-28 19:46:38 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './errors/custom-error';
 export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
 export * from './errors/request-validation-error';
+export * from './errors/not-found-error';
 
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
